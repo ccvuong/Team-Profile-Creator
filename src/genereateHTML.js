e40db087dccc0d5b83cc2ab22e32938da5f24eb9
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-04-24 00:01:07 -0700

    updating HTML files

--- src/genereateHTML.js
+++ src/genereateHTML.js
@@ -6,6 +6,7 @@ const generateHTML = (employee) => {
         switch (employee[i].getRole()) {
             case 'Engineer':
                 newTeamMember += `
+
             <!-- Engineer team member card -->
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
@@ -26,6 +27,7 @@ const generateHTML = (employee) => {
 
             case 'Intern':
                 newTeamMember += `   
+
             <!-- Intern team member card -->
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
@@ -57,42 +59,42 @@ const generateHTML = (employee) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
                 integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="../dist/styles.css" />
+        
+            <link rel="stylesheet" href="./style.css" />
+            <script src="https://kit.fontawesome.com/3ff242710e.js" crossorigin="anonymous"></script>
             <script src="https://kit.fontawesome.com/3ff242710e.js" crossorigin="anonymous"></script>
             <title>The Team</title>
             </head>
 
             <body>
-            <!-- jumbotron -->
-            <div id="jumbotron" class="jumbotron jumbotron-fluid">
-                <div class="container">
-                <h1 class="display-1">The Team</h1>
-                </div>
-            </div>
-
-            <!-- card container -->
-            <div class="container" id="card-container">
-                <div class="row">
-                <div class="card-area col-12 d-flex justify-content-center mt-5">
-
-            <!-- Manager team member card -->
-            <div class="card text" style="width: 18rem;">
-                <div class="card-body">
-                    <h3 class="card-title text-center" id="name">${employee[0].name}</h3>
-                    <h5 id="role" class="text-center"><i class="fas fa-user-tie"></i> Manager</h5>
+            
+                <div id="jumbotron" class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                    <h1 class="display-1">The Team</h1>
                 </div>
 
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item" id="id">ID: ${employee[0].id}</li>
-                    <li class="list-group-item" id="email"><a href="mailto:${employee[0].email}">Email: ${employee[0].email}</a></li>
-                    <li class="list-group-item" id="officeNum">Office number: ${employee[0].officeNum}</li>
-                </ul>
-            </div>
+                
+                <div class="container" id="card-container">
+                    <div class="row">
+                    <div class="card-area col-12 d-flex justify-content-center mt-5">
+                </div>
+                
+                <div class="card text" style="width: 18rem;">
+                    <div class="card-body">
+                        <h3 class="card-title text-center" id="name">${employee[0].name}</h3>
+                        <h5 id="role" class="text-center"><i class="fas fa-user-tie"></i> Manager</h5>
+                    </div>
+                </div>
+                
+                <div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" id="id">ID: ${employee[0].id}</li>
+                        <li class="list-group-item" id="email"><a href="mailto:${employee[0].email}">Email: ${employee[0].email}</a></li>
+                        <li class="list-group-item" id="officeNum">Office number: ${employee[0].officeNum}</li>
+                    </ul>
+                </div>
 
-                ${newTeamMember}
+                    ${newTeamMember}
 
             </body>
 
